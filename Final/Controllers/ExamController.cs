350aa1902b5cb4e42c588b6e21daeba412f0f432
月太げつたい <me@moontai0724.tw>
2021-06-24 19:06:06 +0800

    feat: weekday query

--- Final/Controllers/ExamController.cs
+++ Final/Controllers/ExamController.cs
@@ -13,5 +13,21 @@ namespace Final.Controllers
         {
             return View();
         }
+
+        public ActionResult Weekday()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Weekday(int weekday)
+        {
+            if (1 <= weekday && weekday <= 7)
+            {
+                string[] chinese = { "一", "二", "三", "四", "五", "六", "日" };
+                ViewBag.weekdayInChinese = "星期" + chinese[weekday - 1];
+            }
+            return View();
+        }
     }
 }
